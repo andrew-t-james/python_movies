ddc5711dbf69b900d65c629f0917445266b98103
Andrew James <ajames20@gmail.com>
2017-09-28 13:47:22 +0300

    add comments for functions and methods

--- media.py
+++ media.py
@@ -6,10 +6,12 @@ class Movie():
     """This class provides a way to store movie related information"""
 
     def __init__(self, movie_title, movie_storyline, poster_image, youtube_url):
+        """This Constructor builds each instance of the Movie Class"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = youtube_url
 
     def show_trailer(self):
+        """This Method opens teh youtube url in the Modal window"""
         webbrowser.open(self.trailer_youtube_url)
